f9da21f6a8f0fe7ddda6002f7ba2920e47c2becb
James0812 <james.roussel008@gmail.com>
2026-04-21 11:06:27 +0200

    Add the code

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+        print(f"    method {method}: {count}")
 
     # status check
     status_count = collection.count_documents({
