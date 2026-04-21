49d79896e1b3a140f500915cc90423d70fafbc99
James0812 <james.roussel008@gmail.com>
2026-04-21 11:08:44 +0200

    Add the code

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python3
-"""Script that provides stats about Nginx logs stored in MongoDB"""
+"""log stats from collection
+"""
 from pymongo import MongoClient
+
 
 METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
 
 def log_stats(mongo_collection, option=None):
-    """Script that provides some stats about Nginx logs stored in MongoDB"""
+    """ script that provides some stats about Nginx logs stored in MongoDB
+    """
+    items = {}
     if option:
-        value = mongo_collection.count_documents({"method": option})
+        value = mongo_collection.count_documents(
+            {"method": {"$regex": option}})
         print(f"\tmethod {option}: {value}")
         return
 
-    result = mongo_collection.count_documents({})
+    result = mongo_collection.count_documents(items)
     print(f"{result} logs")
     print("Methods:")
     for method in METHODS:
-        log_stats(mongo_collection, method)
-
-    status_check = mongo_collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
+        log_stats(nginx_collection, method)
+    status_check = mongo_collection.count_documents({"path": "/status"})
     print(f"{status_check} status check")
 
 
