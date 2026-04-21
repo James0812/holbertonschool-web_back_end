19d6e0fdf5c47132f16c1dd1eeaf660489f7db76
James0812 <james.roussel008@gmail.com>
2026-04-21 11:15:01 +0200

    Add the code

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python3
-"""Write a Python script that provides some stats about Nginx logs stored in MongoDB:"""
+"""
+Script that provides statistics about Nginx logs stored in MongoDB
+"""
 
 from pymongo import MongoClient
 
-
 if __name__ == "__main__":
-    client = MongoClient("mongodb://127.0.0.1:27017")
-    collection = client.logs.nginx
+    # Connexion à la base MongoDB
+    client = MongoClient()
+    db = client.logs
+    collection = db.nginx
 
+    # Nombre total de logs
     total_logs = collection.count_documents({})
     print(f"{total_logs} logs")
 
-    print("Methods:")
+    # Nombre de documents par méthode HTTP
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
+    print("Methods:")
     for method in methods:
         count = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    status_check = collection.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status_check} status check")
+    # Nombre de "status checks" (method=GET et path=/status)
+    status_count = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_count} status check")
