cd035117bca6b6fd661a6268aac88e9dec319526
James0812 <james.roussel008@gmail.com>
2026-04-27 10:54:01 +0200

    add the code

--- ES6_basic/1-block-scoping.js
+++ ES6_basic/1-block-scoping.js
@@ -2,8 +2,8 @@ export default function taskBlock(trueOrFalse) {
   let task = false;
   let task2 = true;
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    let task = true;    // block-scopée, n'écrase PAS l'extérieur
+    let task2 = false;  // block-scopée, n'écrase PAS l'extérieur
   }
-  return [task, task2];
+  return [task, task2]; // retourne toujours [false, true]
 }
