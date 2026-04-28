c27eb7e25fd93498e70e3d1466ba0cba7990d35e
James0812 <james.roussel008@gmail.com>
2026-04-28 09:31:59 +0200

    add the code

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,28 @@
+export default class Building {
+  constructor(sqft) {
+    this._checkSqft(sqft);
+    this._sqft = sqft;
+
+    // Vérifie si la classe est étendue
+    if (this.constructor !== Building) {
+      // Vérifie si la méthode est implémentée
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage'
+        );
+      }
+    }
+  }
+
+  // GETTER
+  get sqft() {
+    return this._sqft;
+  }
+
+  // VALIDATION
+  _checkSqft(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+  }
+}
