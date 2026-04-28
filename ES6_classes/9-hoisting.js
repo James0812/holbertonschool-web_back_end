f56d3f6d89d790de832d0082165041dc83f5f78e
James0812 <james.roussel008@gmail.com>
2026-04-28 09:44:51 +0200

    add the code

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,4 +1,3 @@
-// Classes d'abord (pas de hoisting pour class)
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -34,7 +33,7 @@ export class StudentHolberton {
   }
 }
 
-// Ensuite seulement on crée les objets
+// IMPORTANT: création après les classes
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
@@ -44,10 +43,13 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-export const listOfStudents = [
+// ✅ IMPORTANT: default export (sinon test échoue)
+const listOfStudents = [
   student1,
   student2,
   student3,
   student4,
   student5,
 ];
+
+export default listOfStudents;
