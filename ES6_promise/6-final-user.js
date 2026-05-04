283839a822a8c4f1ab8664e35ffb38f81c3a3d5a
James0812 <james.roussel008@gmail.com>
2026-05-04 11:27:21 +0200

    add the code

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,16 +1,14 @@
-import signUpUser from "./4-user-promise.js";
-import uploadPhoto from "./5-photo-reject.js";
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
   ]).then((results) =>
-    results.map((res) => {
-      return {
-        status: res.status,
-        value: res.status === "fulfilled" ? res.value : res.reason,
-      };
-    })
+    results.map((o) => ({
+      status: o.status,
+      value: o.status === 'fulfilled' ? o.value : String(o.reason),
+    }))
   );
 }
